cac4f04324e806874a6ed1998a4670da7543c147
zhangfan <87074799@qq.com>
2017-03-09 11:27:28 +0800

    #2调整tab结构,合并action reducers

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,68 +1,25 @@
 let actions = {
-	request_index: () => (dispatch, getState) => {
-		let url = 'https://cnodejs.org/api/v1/topics';
+	request_topic: (tab, page = 1, limit = 15) => (dispatch, getState) => {
+		let url = `https://cnodejs.org/api/v1/topics?tab=${tab}&page=${page}&limit=${limit}`;
+		if (getState().isFetching) return
+		console.log(url)
+		dispatch(actions.requestTopic(tab))
 		fetch(url).then(function(res) {
 			return res.json()
 		}).then(function(data) {
-			console.log(data)
-			dispatch(actions.requestIndex(data.data))
+			dispatch(actions.receiveTopic(tab, data.data, page, limit))
 		}).catch(e => console.log(e))
 	},
-	request_good: () => (dispatch, getState) => {
-		let url = 'https://cnodejs.org/api/v1/topics?tab=good'
-		fetch(url).then(function(res) {
-			return res.json()
-		}).then(function(data) {
-			console.log(data)
-			dispatch(actions.requestIndex(data.data))
-		}).catch(e => console.log(e))
-	},
-	request_share: () => (dispatch, getState) => {
-		let url = 'https://cnodejs.org/api/v1/topics?tab=share'
-		fetch(url).then(function(res) {
-			return res.json()
-		}).then(function(data) {
-			console.log(data)
-			dispatch(actions.requestIndex(data.data))
-		}).catch(e => console.log(e))
-	},
-	request_ask: () => (dispatch, getState) => {
-		let url = 'https://cnodejs.org/api/v1/topics/?tab=ask'
-		fetch(url).then(function(res) {
-			return res.json()
-		}).then(function(data) {
-			console.log(data)
-			dispatch(actions.requestIndex(data.data))
-		}).catch(e => console.log(e))
-	},
-	request_job: () => (dispatch, getState) => {
-		let url = 'https://cnodejs.org/api/v1/topics?tab=job'
-		fetch(url).then(function(res) {
-			return res.json()
-		}).then(function(data) {
-			console.log(data)
-			dispatch(actions.requestIndex(data.data))
-		}).catch(e => console.log(e))
-	},
-	requestIndex: data => ({
-		type: 'REQUEST_INDEX',
-		playload: data
+	requestTopic: (tab) => ({
+		type: 'REQUEST_TOPICS',
+		tab
 	}),
-	requestGood: data => ({
-		type: 'REQUEST_INDEX',
-		playload: data
-	}),
-	requestShare: data => ({
-		type: 'REQUEST_INDEX',
-		playload: data
-	}),
-	requestAsk: data => ({
-		type: 'REQUEST_INDEX',
-		playload: data
-	}),
-	requestJob: data => ({
-		type: 'REQUEST_INDEX',
-		playload: data
+	receiveTopic: (tab, topics, page, limit) => ({
+		type: 'RECEIVE_TOPICS',
+		tab,
+		topics,
+		page,
+		limit
 	})
 }
 export default actions
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,14 +2,88 @@ import React from 'react';
 
 import Header from '../components/Homepage/Header/index';
 import List from '../components/Homepage/List/index';
+import {
+	Tabs,
+	WhiteSpace
+} from 'antd-mobile';
+const TabPane = Tabs.TabPane;
+let myAction, mYdispatch; //保存action和dispatch
+let allStats = true;
+let goodStats = true;
+let asktats = true;
+let jobStats = true;
+let shareStats = true;
 
-/**
- * A simple example of `BottomNavigation`, with three labels and icons
- * provided. The selected `BottomNavigationItem` is determined by application
- * state (for instance, by the URL).
- */
+let tab = [{
+	name: '全部',
+	tab: '',
+	key: 1
+}, {
+	name: '精华',
+	tab: 'good',
+	key: 2
+}, {
+	name: '分享',
+	tab: 'share',
+	key: 3
+}, {
+	name: '问答',
+	tab: 'ask',
+	key: 4
+}, {
+	name: '招聘',
+	tab: 'job',
+	key: 5
+}]
 class BottomNavigationExampleSimple extends React.Component {
 	componentDidMount() {
+		let {
+			actions,
+			dispatch
+		} = this.props
+			//console.log(actions)
+			//console.log('我的天啊')
+			//console.log(dispatch)
+		dispatch(actions.request_topic(''))
+	}
+
+	callback(key) {
+		switch (key) {
+			case '1':
+				//if (allStats) {
+				mYdispatch(myAction.request_topic(''));
+				//allStats = false
+				//}
+				break;
+			case '2':
+				//if (goodStats) {
+				console.log('不要请求啦');
+				mYdispatch(myAction.request_topic('good'));
+				//goodStats = false;
+				//};
+				break;
+			case '3':
+				//mYdispatch(myAction.request_share())
+				//if (shareStats) {
+				mYdispatch(myAction.request_topic('share'));
+				//shareStats = false
+				//}
+				break;
+			case '4':
+				//mYdispatch(myAction.request_ask())
+				//if (askStats) {
+				mYdispatch(myAction.request_topic('ask'));
+				//askStats = false
+				//}
+				break;
+			case '5':
+				//mYdispatch(myAction.request_job())
+				//if (jobStats) {
+				mYdispatch(myAction.request_topic('job'));
+				//jobStats = false
+				//}
+				break;
+		}
 
 	}
 	render() {
@@ -17,11 +91,25 @@ class BottomNavigationExampleSimple extends React.Component {
 			state,
 			actions,
 			dispatch
-		} = this.props
+		} = this.props;
+		myAction = actions;
+		mYdispatch = dispatch;
+
 		return (
 			<div style={{height:document.documentElement.clientHeight-99}}>
 				<Header/>
-				<List state={state} actions={actions} dispatch={dispatch}/>				
+      			<div>
+				    <Tabs defaultActiveKey="1" onChange={this.callback}>
+						{tab.map(function(index){
+							return (
+								<TabPane tab={index.name} key={index.key}>
+				       				<List state={state}/>
+				      			</TabPane>
+							)
+						})}
+				    </Tabs>
+    				<WhiteSpace />
+  				</div>
 			</div>
 		);
 	}
