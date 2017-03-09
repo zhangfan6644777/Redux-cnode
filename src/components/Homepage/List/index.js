46936b90ed7976fde0d1eff5412e70225fc40969
zhangfan <87074799@qq.com>
2017-03-09 13:38:53 +0800

    #3tab加载动画

--- src/components/Homepage/List/index.js
+++ src/components/Homepage/List/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {
   List
 } from 'antd-mobile';
-
+import FlipMove from 'react-flip-move'
 const Item = List.Item;
 const Brief = Item.Brief;
+
 
 require('./index.less');
 class list extends React.Component {
@@ -15,31 +16,31 @@ class list extends React.Component {
     let {
       state
     } = this.props
-    let all = state.topics.map(function(index) {
-      return (
-        <div key={index.id} className='index-list'>
-                <div>
-                  <img src={index.author.avatar_url}/>
-                  <p>{index.author.loginname}</p>
-                </div>
-                <div>
-                  <p>{index.title}</p>
-                  <p>{index.reply_count}/{index.visit_count}</p>
-                  <p>创建于{index.create_at.substring(0,10)}</p>
-                </div>
-              </div>
-      )
-    })
+      //let disableAllAnimations = topics.length === 20 ? false : true
+      // disableAllAnimations从启用到禁用时enterAnimation设定的动画会不起作用，原因不明。
+    let enterAnimation = {
+      from: {
+        transform: 'translateY(-80px)',
+        opacity: 0
+      },
+      to: {
+        transform: 'translateY(0)',
+        opacity: 1
+      }
+    }
     return (
       <div>
-          <List renderHeader={() => '右侧自定义（无内容 / 文字 / 图片）'} className="my-list">
-            <Item>标题文字</Item>
-            <Item arrow="horizontal" onClick={() => {}}>标题文字</Item>
-            <Item extra="内容内容" arrow="horizontal" onClick={() => {}}>标题文字</Item>
-            <Item extra="10:30" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
-              标题文字 <Brief>副标题</Brief>
-            </Item>
-          </List>
+          <List  className="my-list">
+            <FlipMove enterAnimation={enterAnimation} easing='ease-out' duration='400' staggerDelayBy='40' staggerDurationBy='4'>
+              {state.topics.map(function(index){
+                return(
+                  <Item key={index.id} extra={index.create_at.substring(0,10)} align="bottom" thumb={index.author.avatar_url} multipleLine>
+                    {index.title}<Brief>{index.reply_count}/{index.visit_count} 分享</Brief>
+                  </Item>
+                )
+              })}
+            </FlipMove>
+          </List>  
       </div>
     )
   }
